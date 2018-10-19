9d2eb4ee4e180cd6139ce0ee6dae2a51ce2ce033
Sackfield <will.sackfield@sothebys.com>
2018-10-18 23:01:03 -0400

    Add start of CNN scraper

---
+++ news/settings.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+BOT_NAME = 'news'
+
+SPIDER_MODULES = ['news.spiders']
+NEWSPIDER_MODULE = 'news.spiders'
+
+ROBOTSTXT_OBEY = True
+HTTPCACHE_ENABLED = True
+
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy_splash.SplashCookiesMiddleware': 723,
+    'scrapy_splash.SplashMiddleware': 725,
+    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+}
+
+SPLASH_URL = 'http://localhost:8050/'
+
+SPIDER_MIDDLEWARES = {
+    'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+}
+
+DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
+HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
---
+++ news/spiders/news.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy_splash import SplashRequest
+
+
+class NewsSpider(scrapy.Spider):
+    name = "news"
+    allowed_domains = ["cnn.com"]
+    start_urls = [
+        #'http://www.cnn.com'
+        'https://www.cnn.com/2018/10/18/health/canada-legal-pot-ticket-trnd'
+    ]
+    removeableParagraphs = [
+        u'Paid Content',
+        u'More from CNN',
+        u'Read More',
+        u'Recommended by',
+        u'READ MORE:'
+    ]
+    badImgs = [
+        'outbrain',
+        'data:image',
+        'cnnnext'
+    ]
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield SplashRequest(url, self.parse,
+                endpoint='render.html',
+                args={'wait': 0.5},
+            )
+
+    def parse(self, response):
+        '''
+        for article in response.xpath("//article"):
+            for url in article.xpath("//a/@href").extract():
+                yield scrapy.Request(response.urljoin(url), callback=self.parse)
+        for li in response.css("li.ob-dynamic-rec-container"):
+            for url in li.xpath("//a/@href").extract():
+                yield scrapy.Request(response.urljoin(url), callback=self.parse)
+        '''
+        items = []
+        for article in response.css("article"):
+            item = {}
+            for property in article.xpath('.//*[@itemprop]'):
+                propertyContent = property.xpath("@content").extract_first()
+                propertyName = property.xpath("@itemprop").extract_first()
+                if propertyContent != None:
+                    item[propertyName] = propertyContent
+                else:
+                    if propertyName == u'articleBody':
+                        paragraphs = []
+                        for paragraphDiv in property.css('div.zn-body__paragraph'):
+                            # TODO: Find out how to extract links
+                            for paragraph in paragraphDiv.xpath(".//text()"):
+                                print(paragraph)
+                                strippedParagraph = paragraph.extract().strip()
+                                if len(strippedParagraph) > 0 and strippedParagraph not in self.removeableParagraphs:
+                                    paragraphs.append(strippedParagraph)
+                        item[propertyName] = paragraphs
+            imgs = []
+            for img in article.css('img::attr(src)').extract():
+                fullImg = response.urljoin(img)
+                if not self.isBadImg(fullImg) and fullImg not in imgs:
+                    imgs.append(fullImg)
+            if item:
+                item['imgs'] = imgs
+                items.append(item)
+        if len(items) > 0:
+            yield {
+                'items' : items
+                #'html': response.text
+            }
+
+    def isBadImg(self, img):
+        for badImg in self.badImgs:
+            if badImg in img:
+                return True
+        return False
