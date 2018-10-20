6b9a313c82e62ca79786c6e019207028c3fca62b
Sackfield <will.sackfield@sothebys.com>
2018-10-20 09:12:40 -0400

    Add docstring to items

--- news/items.py
+++ news/items.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
+"""The model for our news items"""
 
 import scrapy
 
