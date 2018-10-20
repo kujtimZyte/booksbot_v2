ac0f8a7b38d4af78a055175db331ef88ef379281
Sackfield <will.sackfield@sothebys.com>
2018-10-20 09:36:08 -0400

    Remove items and pipelines

--- news/items.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""The model for our news items"""
-
-import scrapy
-
-
-class NewsItem(scrapy.Item):
-    """The class that defines the news items fields"""
-    pass
--- news/pipelines.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""The pipeline for our news"""
-
-
-class NewsPipeline(object):
-    """The pipeline for a news site"""
-    def process_item(self, item, spider):
-        return item
