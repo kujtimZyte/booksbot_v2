03a031e850d7f84716bf622cc91fdc2db2f5eb82
Sackfield <will.sackfield@sothebys.com>
2018-11-18 15:37:54 -0500

    Fix return None

--- news/spiders/thedailybeast.py
+++ news/spiders/thedailybeast.py
@@ -6,5 +6,5 @@ from .common import extract_item_from_element_css
 def dailybeast_parse(response):
     """Parses the response from a Daily Beast website"""
     if response.url == 'https://www.thedailybeast.com/':
-        return
+        return None
     return extract_item_from_element_css(response, "div.Body__content")
