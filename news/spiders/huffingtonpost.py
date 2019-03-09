f56a7ef325f8b9dee1f5577398e23095881a1ae8
Sackfield <will.sackfield@sothebys.com>
2019-03-08 22:43:51 -0500

    Fix huffingtonpost article main tag

--- news/spiders/huffingtonpost.py
+++ news/spiders/huffingtonpost.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Parser for the Huffington Post website"""
-from .common import extract_link_id, common_parse
+from .common import extract_link_id, common_parse_return
 
 
 def huffingtonpost_url_parse(url):
@@ -13,15 +13,15 @@ def huffingtonpost_parse(response):
     link_id = huffingtonpost_url_parse(response.url)
     if link_id is None:
         return None, link_id
-    article = common_parse(response, [
+    return common_parse_return(response, [
         {'tag': 'div', 'meta': {'class': 'related-articles'}},
-        {'tag': 'section', 'meta': {'class': 'app-download-interstitial'}}
+        {'tag': 'section', 'meta': {'class': 'app-download-interstitial'}},
+        {'tag': 'section', 'meta': {'class': 'entry__tags'}},
+        {'tag': 'div', 'meta': {'class': 'social-buttons'}}
     ], [
-        {'tag': 'div', 'meta': {'class': 'entry__text'}}
-    ])
-    if article is None:
-        return None, link_id
-    return article.json(), link_id
+        {'tag': 'div', 'meta': {'class': 'entry__text'}},
+        {'tag': 'article', 'meta': {'class': 'entry__content'}}
+    ], link_id)
 
 
 def huffingtonpost_url_filter(_url):
