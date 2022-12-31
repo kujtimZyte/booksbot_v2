131ad27e41ae704c4d6464625233be7f99f71697
Centaurioun <86323645+Centaurioun@users.noreply.github.com>
2022-12-31 23:39:42 +0300

    terms

--- terms/medical_terms.py
+++ terms/medical_terms.py
@@ -1,12 +1,80 @@
-import re
+import scrapy
 
-...
+from terms import pipelines, items
 
-def parse(self, response):
-    for term in response.css("div.col-md-8"):
-        description = term.css("main.mb-3::text").get()
-        description = re.sub(r"[A-Za-z]+ teriminin tıbbi anlamı;", "", description)
-        yield {
-            "name": term.css("h1.display-5::text").get(),
-            "description": description,
-        }
+
+class MedicalTermsSpider(scrapy.Spider):
+	name = "medical_terms"
+	allowed_domains = ["tipterimlerisozlugu.com"]
+	start_urls = [
+		'https://tipterimlerisozlugu.com/',
+	]
+
+	def parse(self, response, **kwargs):
+		for term_url in response.css("div.sres > div.sresl > a ::attr(href)").extract():
+			yield scrapy.Request(response.urljoin(term_url), callback=self.parse_term_page)
+		next_page = response.css("div.sres > div.sresn > a ::attr(href)").extract_first()
+		if next_page:
+			yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
+
+	@staticmethod
+	def parse_term_page(response):
+		item = {'term': response.css("div.sres > div.sresl > a ::text").extract_first(),
+		        'definition': response.css("div.sres > div.sresd ::text").extract_first()}
+		yield item
+
+
+var = pipelines.py
+
+
+class TermsPipeline(object):
+	@staticmethod
+	def process_item(item):
+		return item
+
+
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+var = items.py
+
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+
+import scrapy
+
+
+class TermsItem(scrapy.Item):
+	# define the fields for your item here like:
+	# name = scrapy.Field()
+	pass
+
+
+# Path: terms/settings.py
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for terms project
+
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#     http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
+#     http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
+
+BOT_NAME = 'terms'
+
+SPIDER_MODULES = ['terms.spiders']
+NEWSPIDER_MODULE = 'terms.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
